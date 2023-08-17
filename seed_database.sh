fc4223846ea6d22b7462fb53c044e8c254cf8034
Chesney Hardin <chesney.hardin@gmail.com>
2023-08-17 09:30:26 -0500

    all migrations working. fixed order of seed_database

--- seed_database.sh
+++ seed_database.sh
@@ -3,8 +3,8 @@ rm -rf ./rareapi/migrations
 python3 manage.py migrate
 python3 manage.py makemigrations rareapi
 python3 manage.py migrate rareapi
-
 python3 manage.py loaddata users
+python3 manage.py loaddata tokens
 python3 manage.py loaddata rare_users
 python3 manage.py loaddata categories
 python3 manage.py loaddata posts
